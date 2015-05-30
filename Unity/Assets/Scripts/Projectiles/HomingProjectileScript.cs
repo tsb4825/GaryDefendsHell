74ae4330a64be3f9a90f1a8c555265212f5b5ac9
tsb4825 <tim.byars@live.com>
2015-05-29 22:59:41 -0500

    Curvature of homing missle path.  Not 100% happy with it but will see how it works in actual level design

--- Unity/Assets/Scripts/Projectiles/HomingProjectileScript.cs
+++ Unity/Assets/Scripts/Projectiles/HomingProjectileScript.cs
@@ -4,32 +4,22 @@ using System.Collections;
 public class HomingProjectileScript : ProjectileScript
 {
     public Transform Target;
-    public Vector3 LastTargetPosition;
-    public bool HasTargetChanged;
+    public float MaxAngleTurn;
+    public float Force;
 
     // Update is called once per frame
     void Update()
     {
         if (Target != null)
         {
-            transform.position += (Target.position - transform.position).normalized * Time.deltaTime * ProjectileSpeed;
-            LastTargetPosition = Target.position;
-            HasTargetChanged = true;
+            var angle = Vector3.Lerp(transform.forward, Target.position, Time.deltaTime * MaxAngleTurn);
+            Debug.Log(angle);
+            transform.eulerAngles = angle;
+            transform.GetComponent<Rigidbody2D>().AddForce((Target.position - transform.position).normalized * Force);
         }
         else
         {
             Target = TowerFiredFrom.GetComponent<HomingTowerScript>().Target;
-
-            if (Target == null && HasTargetChanged)
-            {
-                UtilityFunctions.DebugMessage("Force Added. " + (LastTargetPosition - transform.position).normalized * 50 * ProjectileSpeed);
-                transform.GetComponent<Rigidbody2D>().AddForce((LastTargetPosition - transform.position).normalized * 50 * ProjectileSpeed);
-                HasTargetChanged = false;
-            }
-            else if (Target != null)
-            {
-                HasTargetChanged = true;
-            }
         }
     }
 }
--- Unity/Assets/Scripts/Towers/HomingTowerScript.cs
+++ Unity/Assets/Scripts/Towers/HomingTowerScript.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class HomingTowerScript : Tower
 {
-
     public Transform Projectile;
+    public float Force;
+
 
     public override void Fire()
     {
@@ -14,5 +15,7 @@ public class HomingTowerScript : Tower
         Transform bullet = (Transform)Instantiate(Projectile, cannonPosition, Quaternion.identity);
         bullet.GetComponent<HomingProjectileScript>().Target = Target;
         bullet.GetComponent<HomingProjectileScript>().TowerFiredFrom = transform;
+        bullet.LookAt(Target);
+        bullet.GetComponent<Rigidbody2D>().AddForce((Target.position - bullet.position).normalized * Force);
     }
 }
